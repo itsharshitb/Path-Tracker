1f19a2b6dd6e73f554be014177d4275a315962be
Harshit B <harshitbhatt972@gmail.com>
2020-10-19 20:47:09 +0530

    small change

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -3,41 +3,39 @@ import { Accuracy, requestPermissionsAsync, watchPositionAsync } from "expo-loca
 
 export default (shouldTrack, callback) => {
     const [err, setErr] = useState(null);
-    const [subscriber, setSubscriber] =useState(null);
-
-    const startWatching = async () => {
-        try{
-        await requestPermissionsAsync();
-        const sub = await watchPositionAsync({
-          accuracy: Accuracy.BestForNavigation,
-          timeInterval: 1000,
-          distanceInterval: 10 
-        },
-        callback 
-        );
-        setSubscriber(sub);
-    } catch(e){
-        setErr(e);
-        //console.log(err);
-        }
-    };
 
     useEffect(() => {
+        let subscriber;
+          const startWatching = async () => {
+            try{
+            await requestPermissionsAsync();
+            const subscriber = await watchPositionAsync({
+              accuracy: Accuracy.BestForNavigation,
+              timeInterval: 1000,
+              distanceInterval: 10 
+            },
+            callback 
+            );
+        } catch(e){
+            setErr(e);
+            //console.log(err);
+            }
+        };
         if(shouldTrack)
         {
             startWatching();
         }
         else {
-            subscriber.remove();
-            setSubscriber(null);
+            if(subscriber){
+                subscriber.remove();
+            }
+            subscriber = null;
         }
-
         return () => {
             if(subscriber){
                 subscriber.remove();
             }
         }
       }, [shouldTrack, callback]);
-      
       return [err];
 };
