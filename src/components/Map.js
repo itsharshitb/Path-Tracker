91389a0c1f0b9fa9ca0cae4e96aa09f6e238d189
Harshit B <harshitbhatt972@gmail.com>
2020-10-22 13:39:54 +0530

    error files

--- src/components/Map.js
+++ src/components/Map.js
@@ -5,9 +5,13 @@ import {Context as LocationContext} from "../context/LocationContext";
 
 const Map = () => {
 <<<<<<< HEAD
+<<<<<<< HEAD
   const {state: { currentLocation, locations }} = useContext(LocationContext);
   console.log(locations);
 
+=======
+  const {state: { currentLocation }} = useContext(LocationContext);
+>>>>>>> parent of 71719a6... added polyline in map.js
 =======
   const {state: { currentLocation }} = useContext(LocationContext);
 >>>>>>> parent of 71719a6... added polyline in map.js
@@ -29,7 +33,10 @@ const Map = () => {
       fillColor="rgba(255, 0, 0, 0.3)"
       />
 <<<<<<< HEAD
+<<<<<<< HEAD
       <Polyline coordinates={locations.map(loc => loc.coords)} />
+=======
+>>>>>>> parent of 71719a6... added polyline in map.js
 =======
 >>>>>>> parent of 71719a6... added polyline in map.js
       </MapView>
